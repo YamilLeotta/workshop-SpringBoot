c90d251f3bcfb38ff2774fccee3802404c8ae211
Yamil Leotta <108009904+YamilLeotta@users.noreply.github.com>
2023-10-24 18:47:30 -0300

    feat(Clients/CRUD): add models

---
+++ src/main/java/grupo/artifact/model/Company.java
@@ -0,0 +1,42 @@
+package grupo.artifact.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String cuit;
+
+    @Column(nullable = false)
+    private Float start_date;
+
+    @OneToMany(mappedBy = "company")
+    private Set<Client> clients;
+}
